c0909ee5121fa8e723eb4412d81c69f43bef82e7
Yiwei Zhang <zhangyiwei79@gmail.com>
2010-07-09 16:04:13 -0400

    Week 7, add inverse wavelet transform

--- wavelet.cpp
+++ wavelet.cpp
@@ -2,6 +2,12 @@
 
 #include "wavelet.h"
 
+//Daubechies 2 wavelet basis
+double pLoFilter[4] = {0.4830, 0.8365, 0.2241, -0.1294};
+double pHiFilter[4] = {-0.1294,-0.2241, 0.8365, -0.4830};
+double pRecHiFilter[4] = {-0.4830, 0.8365, -0.2241, -0.1294};
+double pRecLoFilter[4] = {-0.1294,0.2241, 0.8365, 0.4830};
+
 //Perform column convlolution
 void ColConvolution2D(double *pSrc, double *pFilter, int filterLength, int row, int col, double *pRes, bool bAddZero)
 {
@@ -73,7 +79,7 @@ void ColConvolution2D(double *pSrc, double *pFilter, int filterLength, int row, 
 }
 
 //Perform row convlolution
-void RowConvolution2D(double *pSrc, double *pFilter, int filterLength, int row, int col, double *pRes, bool bAddZero)
+void RowConvolution2D(double *pSrc, double *pFilter, int filterLength, int row, int col, double *pRes, bool bAddZero, bool bAddResult)
 {
 	double temp, a, b;
 	double *pData;
@@ -135,7 +141,14 @@ void RowConvolution2D(double *pSrc, double *pFilter, int filterLength, int row, 
 			    temp = temp + a*b;
 		    }
 		
-		    *pRes = temp;
+			if (bAddResult)
+			{
+				*pRes = *pRes + temp;
+			}
+			else
+			{
+		        *pRes = temp;
+			}
 			pRes++;
 		}
 
@@ -145,6 +158,129 @@ void RowConvolution2D(double *pSrc, double *pFilter, int filterLength, int row, 
 	free(pData);
 }
 
+
+
+
+void IDWT_Partial(double *pSrc, int row, int col, int orig_row, int orig_col, double *pFilter1, double *pFilter2, int filterLen, double *pResult)
+{
+	int count = 0;
+
+	double *pTemp1 = (double *)malloc(sizeof(double)*(2*row-1)*col);
+	double *pTemp2 = (double *)malloc(sizeof(double)*(2*row-1+filterLen-1)*col);
+	double *pTemp3 = (double *)malloc(sizeof(double)*(2*row-1+filterLen-1)*(2*col-1));
+	
+
+	//Up Sample rows
+	for (int i=0; i<2*row-1; i++)
+	{
+		for (int j=0; j<col; j++)
+		{
+			if (i%2 == 1)
+			{
+				*(pTemp1+i*col+j) = 0;
+			}
+			else
+			{
+				*(pTemp1+i*col+j) = *(pSrc+i/2*col+j);
+			}
+		}
+	}
+
+	//ShowResult(pTemp1, 2*row-1, col);
+	ColConvolution2D(pTemp1, pFilter1, filterLen, 2*row-1, col, pTemp2, true);
+	//ShowResult(pTemp2, 2*row-1+filterLen-1,col);
+
+	//Up Sample cols
+	count = 2*row-1+filterLen-1;
+	for (int i=0; i<count; i++)
+	{
+		for (int j=0; j<(2*col-1); j++)
+		{
+			if (j%2 == 1)
+			{
+				*(pTemp3+i*(col*2-1)+j) = 0;
+			}
+			else
+			{
+				*(pTemp3+i*(col*2-1)+j) = *(pTemp2 + i*col + j/2);
+			}
+		}
+	}
+
+	RowConvolution2D(pTemp3, pFilter2, filterLen, count, (2*col-1), pResult, true, true);
+
+	free(pTemp1);
+	free(pTemp2);
+	free(pTemp3);
+
+}
+
+//Perform discrete 2 dimensinal inverse wavelet transform
+void InverseWaveletTransform2D(double *pLow, double *pHor, double *pVer, double *pDiag,  int row, int col, int orig_row, int orig_col, double *pLoFilter, double *pHiFilter, int filterLen, double *pResult)
+{
+	int row_shift = 0;
+	int col_shift = 0;
+	int index = 0;
+
+	double *pTemp = (double *)malloc(sizeof(double)*(2*row-1+filterLen-1)*(2*col-1+filterLen-1));
+	memset(pTemp, 0, sizeof(double)*(2*row-1+filterLen-1)*(2*col-1+filterLen-1));
+
+	IDWT_Partial(pLow,  row, col, orig_row, orig_col, pLoFilter, pLoFilter, filterLen, pTemp);
+	IDWT_Partial(pHor,  row, col, orig_row, orig_col, pHiFilter, pLoFilter, filterLen, pTemp);
+	IDWT_Partial(pVer,  row, col, orig_row, orig_col, pLoFilter, pHiFilter, filterLen, pTemp);
+	IDWT_Partial(pDiag, row, col, orig_row, orig_col, pHiFilter, pHiFilter, filterLen, pTemp);
+
+	ShowResultInt(pTemp, 2*row-1+filterLen-1, 2*col-1+filterLen-1);
+
+	if ((orig_row + filterLen - 1)%2 == 0)
+	{
+		row_shift = filterLen - 2;
+	}
+	else
+	{
+		if (row*2 < (orig_row + filterLen - 1))
+		{
+			row_shift = filterLen - 2;
+		}
+		else
+		{
+			row_shift = filterLen - 1;
+		}
+	}
+
+	if ((orig_col + filterLen - 1)%2 == 0)
+	{
+		col_shift = filterLen - 2;
+	}
+	else
+	{
+		if (col*2 < (orig_col + filterLen - 1))
+		{
+			col_shift = filterLen - 2;
+		}
+		else
+		{
+			col_shift = filterLen - 1;
+		}
+	}
+	col = 2*col-1+filterLen-1;
+	index = col*row_shift + col_shift;
+
+	//Keep the center part as result
+	for (int i=0; i<orig_row; i++)
+	{
+		for (int j=0; j<orig_col; j++)
+		{
+			*(pResult+i*orig_col+j) = *(pResult+i*orig_col+j) + *(pTemp+index);
+			index++;
+		}
+		index = index + (col-orig_col);
+	}
+
+	free(pTemp);
+}
+
+//Perform discrete 2 dimensinal wavelet transform
 void WaveletTransform2D(double *pSrc, int row, int col, double *pLoFilter, double *pHiFilter, int filterLen, double *pLow, double *pVer, double *pHor, double *pDiag)
 {
 	int nIndex = 0;
@@ -154,7 +290,7 @@ void WaveletTransform2D(double *pSrc, int row, int col, double *pLoFilter, doubl
 	double *pTemp1 = (double *)malloc(sizeof(double)*row*coeff_col);
 	double *pTemp2 = (double *)malloc(sizeof(double)*coeff_row*coeff_col);
 	
-	RowConvolution2D(pSrc, pLoFilter, filterLen, row, col, pTemp1, false);
+	RowConvolution2D(pSrc, pLoFilter, filterLen, row, col, pTemp1, false, false);
 	
 	//Get the Low frquency part
 	ColConvolution2D(pTemp1, pLoFilter, filterLen, row, col+filterLen-1, pTemp2, false);
@@ -187,9 +323,9 @@ void WaveletTransform2D(double *pSrc, int row, int col, double *pLoFilter, doubl
 		}
 	}
 
-	RowConvolution2D(pSrc, pHiFilter, filterLen, row, col, pTemp1, false);
+	RowConvolution2D(pSrc, pHiFilter, filterLen, row, col, pTemp1, false, false);
 	//Get vertical coefficients
-	ColConvolution2D(pTemp2, pLoFilter, filterLen, row, col+filterLen-1, pTemp2, false);
+	ColConvolution2D(pTemp1, pLoFilter, filterLen, row, col+filterLen-1, pTemp2, false);
 	nIndex = 0;
 	for (int i=0; i<coeff_row; i++)
 	{
